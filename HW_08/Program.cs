13146e8941b76d43eed55c602437ffed5a7b1f4b
Elena Negerish <elenkaNeg@yandex.ru>
2022-09-06 23:13:21 +0300

    HW 8

--- HW_08/Program.cs
+++ HW_08/Program.cs
@@ -137,10 +137,7 @@ int[,] myArray = CreateRandom2DArray(m, n, min, max);
 Show2DArray(myArray);
 MinSumElementsRows(myArray);
 */
-
-
-
-
+/*
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение 
 // двух матриц.
 // Например, даны 2 матрицы:
@@ -152,69 +149,76 @@ MinSumElementsRows(myArray);
 
 void CreateRandom2DArray(int[,] matr1, int[,] matr2)
 {
-    for (int i = 0;  i < matr1.GetLength(0); i++)
+    for (int i = 0; i < matr1.GetLength(0); i++)
         for (int j = 0; j < matr1.GetLength(1); j++)
             matr1[i, j] = new Random().Next(1, 10);
-    
+
     for (int z = 0; z < matr2.GetLength(0); z++)
         for (int k = 0; k < matr2.GetLength(1); k++)
-            matr2[z, k] = new Random().Next(1, 10);      
-   
+            matr2[z, k] = new Random().Next(1, 10);
+
 }
+
 void Show2DArray(int[,] matr1, int[,] matr2)
 {
     for (int i = 0; i < matr1.GetLength(0); i++)
     {
         for (int j = 0; j < matr1.GetLength(1); j++)
-        
+
             Console.Write(matr1[i, j] + " ");
-            Console.Write(" | ");       
-        Console.WriteLine();          
+        Console.Write(" | ");
+        Console.WriteLine();
     }
     for (int z = 0; z < matr2.GetLength(0); z++)
     {
         for (int k = 0; k < matr2.GetLength(1); k++)
-        
+
             Console.Write(matr2[z, k] + " ");
-        
         Console.WriteLine();
-    }       
+    }
+    Console.WriteLine();
 }
 
-int[,] MultiplyingArrays(int [,] matr1, int[,] matr2)
+void ShowArray(int[,] matr1)
+{
+    for (int i = 0; i < matr1.GetLength(0); i++)
     {
-        int[,] newMatrix = new int[matr1.Length, matr2.Length];
+        for (int j = 0; j < matr1.GetLength(1); j++)
 
-        for (int i = 0; i < newMatrix.GetLength(0); i++)
+            Console.Write(matr1[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+int[,] MultiplyingArrays(int[,] matr1, int[,] matr2)
+{
+    int[,] newMatrix = new int[matr1.GetLength(0), matr2.GetLength(0)];
+
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr2.GetLength(1); j++)
         {
-            for (int j = 0; j < newMatrix.GetLength(1); j++)
+            for (int k = 0; k < matr1.GetLength(0); k++)
             {
-                int sum = 0;
-                for (int k = 0; k < matr1.GetLength(1); k++)
-                {
-                    sum = matr1[i, k] * matr2[k, j]; 
-                    newMatrix[i, j]=sum;                 
-                    Console.Write(newMatrix[i, j] + " ");       
-                }
-                Console.WriteLine();  
+                int sum = matr1[i, k] * matr2[k, j];
+                newMatrix[i, j] += sum;
+                Console.Write(sum + " ");
             }
-            
+            Console.WriteLine();
         }
-        return newMatrix;
-    }        
-  
+
+    }
+    return newMatrix;
+}
 int[,] matrix1 = new int[2, 2];
 int[,] matrix2 = new int[2, 2];
 CreateRandom2DArray(matrix1, matrix2);
 Show2DArray(matrix1, matrix2);
-MultiplyingArrays(matrix1, matrix2);
+int[,] result = MultiplyingArrays(matrix1, matrix2);
+Console.WriteLine(" = ");
+ShowArray(result);
 Console.WriteLine();
-
-
-
-
-
-
+*/
+/*
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
@@ -222,6 +226,52 @@ Console.WriteLine();
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
+void CreateRandom3DArray(int [,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(2); k++)
+                array[i, j, k] = new Random().Next(10, 100);
+               
+}
+
+void Show3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+                Console.Write(array[i, j, k] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ShowArrayIndex(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k]);
+                Console.Write("({0},{1},{2})  ",  i, j, k);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,,] matrix = new int [2, 2, 2];
+CreateRandom3DArray(matrix);
+Show3DArray(matrix);
+ShowArrayIndex(matrix);
+*/ 
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
